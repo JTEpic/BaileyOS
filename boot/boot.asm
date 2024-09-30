4403abc6e39a073362dc8a9678943885bfdd6232
JTEpic <jjrocks2006@att.net>
2024-09-30 01:06:49 -0500

    bootable bootloader

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,7 +4,7 @@ BITS 16
 start:
     mov ah, 0eh ;is command
     mov al, 'B'
-    mov bx, 0 ;screen color?
+    mov bx, 0 ;page num, unneeded?
     int 0x10 ;is interupt
 
     jmp $
--- boot/boot2.asm
+++ boot/boot2.asm
@@ -1,7 +1,27 @@
-ORG 0x7c00
+ORG 0 ;0x7c00
 BITS 16
 
+_start:
+    jmp short start
+    nop ;no operation
+
+times 33 db 0 ;bios parameter block, bios wont fill in values
+
 start:
+    jmp 0x7c0:start2 ;ensure code seg location jump
+
+start2:
+    ;setup segment registers to ensure compatibility
+    ;register * 16 + offset (org is offset)
+    cli ;clear interupts
+    mov ax, 0x7c0 ;code segment location
+    mov ds, ax ;DS (Data Segment)
+    mov es, ax ;ES (Extra Segment)
+    mov ax, 0x00 ;correct?
+    mov ss, ax ;SS (Stack Segment)
+    mov sp, 0x7c00 ;stack pointer, setup stack
+    sti ;enable interupts
+
     mov si, message
     call print
     jmp $
@@ -10,14 +30,14 @@ print:
     mov bx, 0
 
     .loop:
-        lodsb
-        cmp al, 0
-        je .done
+        lodsb ;load and increment si in al
+        cmp al, 0 ;check if 0
+        je .done ;call done zero flag (previous if true)
         call print_char
 
         jmp .loop
 
-    .done
+    .done:
         ret
 
 print_char:
